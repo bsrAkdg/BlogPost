1868542be9d4a24519960e481e74ee4acb886b9e
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-26 18:10:50 +0300

    Session manager singleton

--- app/src/main/java/com/bsrakdg/blogpost/session/SessionManager.kt
+++ app/src/main/java/com/bsrakdg/blogpost/session/SessionManager.kt
@@ -3,7 +3,9 @@ package com.bsrakdg.blogpost.session
 import android.app.Application
 import com.bsrakdg.blogpost.persistence.AuthTokenDao
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class SessionManager
 @Inject
 constructor(
