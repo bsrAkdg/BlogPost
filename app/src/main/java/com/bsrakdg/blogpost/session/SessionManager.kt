4eab9600310c9f174a1b4b5c40b1c7bc0f3baccc
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-28 11:43:31 +0300

    Session manager login logout

--- app/src/main/java/com/bsrakdg/blogpost/session/SessionManager.kt
+++ app/src/main/java/com/bsrakdg/blogpost/session/SessionManager.kt
@@ -1,7 +1,19 @@
 package com.bsrakdg.blogpost.session
 
 import android.app.Application
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.bsrakdg.blogpost.models.AuthToken
 import com.bsrakdg.blogpost.persistence.AuthTokenDao
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,4 +25,77 @@ constructor(
     val application: Application
 ) {
 
+    private val TAG: String = "SessionManager"
+
+    private val _cachedToken = MutableLiveData<AuthToken>()
+
+    val cachedToken: LiveData<AuthToken>
+        get() = _cachedToken
+
+
+    fun login(newValue: AuthToken) {
+        setValue(newValue)
+    }
+
+    fun logout() {
+        Log.d(TAG, "logout...")
+
+        GlobalScope.launch(IO) {
+            var errorMessage: String? = null
+            try {
+                // if user logout, remove token from database
+                cachedToken.value!!.account_pk?.let {
+                    authTokenDao.nullifyToken(it)
+                }
+            } catch (e: CancellationException) {
+                Log.e(TAG, "logout : ${e.message}")
+                errorMessage = e.message
+            } catch (e: Exception) {
+                Log.e(TAG, "logout : ${e.message}")
+                errorMessage = errorMessage + "\n" + e.message
+            } finally {
+                errorMessage?.let {
+                    Log.e(TAG, "logout : $it")
+                }
+                Log.d(TAG, "logout : Finally...");
+                setValue(null)
+            }
+        }
+    }
+
+    fun setValue(newValue: AuthToken?) {
+        // We must be sure on main thread
+        GlobalScope.launch(Main) {
+            if (_cachedToken.value != newValue) {
+                _cachedToken.value = newValue!!
+            }
+        }
+    }
+
+    private fun isConnectedToTheInternet(): Boolean {
+        val connectivityManager =
+            application.getSystemService(android.content.Context.CONNECTIVITY_SERVICE)
+                    as ConnectivityManager
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val networkCapabilities = connectivityManager.activeNetwork ?: return false
+                val activeNetwork =
+                    connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+
+                return when {
+                    activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
+                            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    else -> false
+                }
+            } else {
+                return connectivityManager.activeNetworkInfo != null &&
+                        connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "isConnectedToTheInternet: ${e.message}")
+        }
+        return false
+
+    }
 }
