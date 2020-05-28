2f33b1e1d4c473b76099c4812614523c85192233
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-28 13:22:28 +0300

    AutRepository login register attempt and error handling

--- app/src/main/java/com/bsrakdg/blogpost/repository/auth/AuthRepository.kt
+++ app/src/main/java/com/bsrakdg/blogpost/repository/auth/AuthRepository.kt
@@ -1,13 +1,20 @@
 package com.bsrakdg.blogpost.repository.auth
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.switchMap
 import com.bsrakdg.blogpost.api.auth.BlogPostAuthService
-import com.bsrakdg.blogpost.api.auth.network_responses.LoginResponse
-import com.bsrakdg.blogpost.api.auth.network_responses.RegistrationResponse
+import com.bsrakdg.blogpost.models.AuthToken
 import com.bsrakdg.blogpost.persistence.AccountPropertiesDao
 import com.bsrakdg.blogpost.persistence.AuthTokenDao
 import com.bsrakdg.blogpost.session.SessionManager
-import com.bsrakdg.blogpost.utils.GenericApiResponse
+import com.bsrakdg.blogpost.ui.DataState
+import com.bsrakdg.blogpost.ui.Response
+import com.bsrakdg.blogpost.ui.ResponseType
+import com.bsrakdg.blogpost.ui.auth.state.AuthViewState
+import com.bsrakdg.blogpost.utils.ApiEmptyResponse
+import com.bsrakdg.blogpost.utils.ApiErrorResponse
+import com.bsrakdg.blogpost.utils.ApiSuccessResponse
+import com.bsrakdg.blogpost.utils.ErrorHandling.Companion.ERROR_UNKNOWN
 import javax.inject.Inject
 
 class AuthRepository
@@ -19,20 +26,100 @@ constructor(
     val sessionManager: SessionManager
 ) {
 
-    fun testLogin(
+    fun attemptLogin(
         email: String,
         password: String
-    ): LiveData<GenericApiResponse<LoginResponse>> {
-        return blogPostAuthService.login(email, password)
+    ): LiveData<DataState<AuthViewState>> {
+        return blogPostAuthService.login(email, password).switchMap { response ->
+            object : LiveData<DataState<AuthViewState>>() {
+                override fun onActive() {
+                    super.onActive()
+                    when (response) {
+                        is ApiSuccessResponse -> {
+                            value = DataState.data(
+                                data = AuthViewState(
+                                    authToken = AuthToken(
+                                        response.body.pk,
+                                        response.body.token
+                                    )
+                                ),
+                                response = null
+                            )
+                        }
+                        is ApiErrorResponse -> {
+                            value = DataState.error(
+                                response = Response(
+                                    message = response.errorMessage,
+                                    responseType = ResponseType.Dialog()
+                                )
+                            )
+                        }
+                        is ApiEmptyResponse -> {
+                            value = DataState.error(
+                                response = Response(
+                                    message = ERROR_UNKNOWN,
+                                    responseType = ResponseType.Dialog()
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    fun testRegister(
+    fun attemptRegister(
         email: String,
         username: String,
         password: String,
         confirmPassword: String
-    ): LiveData<GenericApiResponse<RegistrationResponse>> {
+    ): LiveData<DataState<AuthViewState>> {
         return blogPostAuthService.register(email, username, password, confirmPassword)
+            .switchMap { response ->
+                object : LiveData<DataState<AuthViewState>>() {
+                    override fun onActive() {
+                        super.onActive()
+                        when (response) {
+                            is ApiSuccessResponse -> {
+                                value = DataState.data(
+                                    data = AuthViewState(
+                                        authToken = AuthToken(
+                                            response.body.pk,
+                                            response.body.token
+                                        )
+                                    ),
+                                    response = null
+                                )
+                            }
+                            is ApiErrorResponse -> {
+                                value = DataState.error(
+                                    response = Response(
+                                        message = response.errorMessage,
+                                        responseType = ResponseType.Dialog()
+                                    )
+                                )
+                            }
+                            is ApiEmptyResponse -> {
+                                value = DataState.error(
+                                    response = Response(
+                                        message = ERROR_UNKNOWN,
+                                        responseType = ResponseType.Dialog()
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
     }
+
+//    fun attemptRegister(
+//        email: String,
+//        username: String,
+//        password: String,
+//        confirmPassword: String
+//    ): LiveData<DataState<AuthViewState>> {
+//        return blogPostAuthService.register(email, username, password, confirmPassword)
+//    }
 }
 
---
+++ app/src/main/java/com/bsrakdg/blogpost/utils/ErrorHandling.kt
@@ -0,0 +1,8 @@
+package com.bsrakdg.blogpost.utils
+
+class ErrorHandling {
+
+    companion object {
+        const val ERROR_UNKNOWN = "Unknown Error"
+    }
+}
