7d830ce825f031e20c4787c8d24bce4092992613
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-06 18:31:41 +0300

    SearchView and swipe refresh

--- app/src/main/java/com/bsrakdg/blogpost/ui/main/blog/BlogFragment.kt
+++ app/src/main/java/com/bsrakdg/blogpost/ui/main/blog/BlogFragment.kt
@@ -1,14 +1,19 @@
 package com.bsrakdg.blogpost.ui.main.blog
 
+import android.app.SearchManager
+import android.content.Context.SEARCH_SERVICE
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bsrakdg.blogpost.R
 import com.bsrakdg.blogpost.models.BlogPost
 import com.bsrakdg.blogpost.ui.DataState
@@ -18,9 +23,13 @@ import com.bsrakdg.blogpost.utils.ErrorHandling
 import com.bsrakdg.blogpost.utils.TopSpacingItemDecoration
 import kotlinx.android.synthetic.main.fragment_blog.*
 
-class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
+class BlogFragment : BaseBlogFragment(),
+    BlogListAdapter.Interaction,
+    SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var recyclerAdapter: BlogListAdapter
+
+    private lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +41,10 @@ class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowTitleEnabled(false)
+
         setHasOptionsMenu(true)
+        swipe_refresh.setOnRefreshListener(this)
 
         initRecyclerView()
         subscribeObservers()
@@ -40,6 +52,18 @@ class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
         if (savedInstanceState == null) {
             viewModel.loadFirstPage()
         }
+    }
+
+    private fun onBlogSearchOrFilter() {
+        viewModel.loadFirstPage().let { // completed loadFirstPage
+            resetUI()
+        }
+    }
+
+    private fun resetUI() {
+        blog_post_recyclerview.smoothScrollToPosition(0)
+        stateChangeListener.hideSoftKeyboard()
+        focusable_view.requestFocus()
     }
 
     private fun subscribeObservers() {
@@ -59,6 +83,43 @@ class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
                 )
             }
         })
+    }
+
+    private fun initSearchView(menu: Menu) {
+        activity?.apply {
+            val searchManager: SearchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+            searchView = menu.findItem(R.id.action_search).actionView as SearchView
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            searchView.maxWidth = Int.MAX_VALUE
+            searchView.setIconifiedByDefault(true)
+            searchView.isSubmitButtonEnabled = true
+        }
+
+
+        // case 1: ENTER ON COMPUTER KEYBOARD OR ARROW ON VIRTUAL KEYBOARD
+        val searchPlate = searchView.findViewById(R.id.search_src_text) as EditText
+        searchPlate.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED
+                || actionId == EditorInfo.IME_ACTION_SEARCH
+            ) {
+                val searchQuery = v.text.toString()
+                Log.e(TAG, "SearchView : (keyboard or arrow) executing search... $searchQuery")
+                viewModel.setQuery(searchQuery).let {
+                    onBlogSearchOrFilter()
+                }
+            }
+            true
+        }
+
+        // case 2: SEARCH BUTTON CLICKED (in toolbar)
+        (searchView.findViewById(R.id.search_go_btn) as View).setOnClickListener {
+            val searchQuery = searchPlate.text.toString()
+            Log.e(TAG, "SearchView : (button) executing search... $searchQuery")
+
+            viewModel.setQuery(searchQuery).let {
+                onBlogSearchOrFilter()
+            }
+        }
     }
 
     private fun handlePagination(dataState: DataState<BlogViewState>) {
@@ -137,5 +198,16 @@ class BlogFragment : BaseBlogFragment(), BlogListAdapter.Interaction {
         // update state event for show selected item
         viewModel.setBlogPost(item)
         findNavController().navigate(R.id.action_blogFragment_to_viewBlogFragment)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.search_menu, menu)
+        initSearchView(menu)
+    }
+
+    override fun onRefresh() {
+        onBlogSearchOrFilter()
+        swipe_refresh.isRefreshing = false
     }
 }
